eefeadd00d6b108f13fc8b1b77c9167e65f65791
bengigenc <genc.bengi@gmail.com>
2022-07-04 12:43:06 +0300

    Create the function to display contact page

---
+++ modules/displayContact.js
@@ -0,0 +1,13 @@
+function displayContainer() {
+    const contactLink = document.querySelector('#contact');
+
+    const listPage = document.querySelector('#display-section');
+    const formPage = document.querySelector('#form-section');
+    const contactPage = document.querySelector('#contact-section');
+
+    contactLink.addEventListener('click', () => {
+        listPage.style.display = 'none';
+        formPage.style.display = 'none';
+        contactPage.style.display = 'flex';
+    });
+}
