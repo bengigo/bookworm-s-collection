753694fe058bd4f6ceb6fe01600496efb7f0545a
bengigenc <genc.bengi@gmail.com>
2022-07-04 13:12:50 +0300

    Import class and functions

---
+++ index.js
@@ -0,0 +1,9 @@
+import Book from "./modules/bookClass.js";
+
+import displayContact from "./modules/displayContact.js";
+
+import displayForm from "./modules/displayForm.js";
+
+import displayList from "./modules/displayList.js";
+
+// import displayTime
