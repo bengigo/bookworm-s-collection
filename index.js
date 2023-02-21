aa63d041b73c3d77cdf64ce1d25a15d0bf27e291
Bengi Genç <genc.bengi@gmail.com>
2023-02-22 00:34:47 +0300

    Change UI layout

--- index.js
+++ index.js
@@ -1,23 +1,156 @@
-import Book from './modules/bookClass.js';
+const addToCollection = document.querySelector('#add-collection');
+const addToWhislist = document.querySelector('#add-whislist')
+const collectionForm = document.querySelector('#collection-form');
+const whislistForm = document.querySelector('#whislist-form')
+const collectionAuthorInput = document.querySelector('#author-collection');
+const collectionTitleInput = document.querySelector('#title-collection');
+const collectionYearInput = document.querySelector('#year-collection');
+const collectionPagesInput = document.querySelector('#pages-collection');
+const readCheck = document.querySelector('#read');
+const notReadCheck = document.querySelector('#not-read');
+let status = '';
+function statusCheck() {
+  if (readCheck.checked) {
+    status = 'Read';
+  }
+  if (notReadCheck.checked) {
+    status = 'Not read';
+  }
+  return status;
+}
+const submitToCollection = document.querySelector('#submit-collection');
+const submitToWhislist = document.querySelector('#submit-whislist')
+const bookList = document.querySelector('#book-list');
+let newBook = {};
 
-import displayContact from './modules/displayContact.js';
+let collection = [];
+collection = JSON.parse(localStorage.getItem('collection') || '[]');
 
-import displayForm from './modules/displayForm.js';
+function Book( title, author, publishYear, numberOfPages, status) {
+  this.title = title;
+  this.author = author;
+  this.publishYear = publishYear;
+  this.numberOfPages = numberOfPages;
+  this.status = status;
+  this.index = collection.length + 1;
+}
 
-import displayList from './modules/displayList.js';
+function displayCollection() {
+  bookList.innerHTML = '';
+  let checkedStatus = '';
+  let collection = [];
+  collection = JSON.parse(localStorage.getItem('collection') || '[]');
+  collection.forEach((book) => {
+    if (book.status === 'Read') {
+      checkedStatus = 'checked';
+    } else {
+      checkedStatus = '';
+    }
+    bookList.innerHTML += `
+    <li class="card">
+      <p>${book.title} by ${book.author}</p>
+      <div>
+        <p>Publish year: ${book.publishYear}</p>
+        <p> Number of pages: ${book.numberOfPages}</p>
+      </div>
+      <div>
+      <div>
+      <p>ebook?</p>
 
-import displayTime from './modules/displayTime.js';
+      <p>${book.status}</p>
+  
+      <p class="switch-input small"><label class="switch"><input class="switch-input"
+       type="checkbox" ${checkedStatus}><span class="slider" id="${book.index}"></span></label></p></div>
+      </div>
+      <p class="medium"><button class="remove" id="${book.index}">remove</button></p>
 
-Book.add();
+    </li>
+    `;
 
-Book.display();
+  });
+}
 
-Book.remove();
+function displayCollectionForm() {
+  collectionForm.classList.add('active');
+}
 
-displayList();
+function displayWhislistForm() {
+  whislistForm.classList.add('active');
+}
 
-displayForm();
+function createBook() {
+  const author = collectionAuthorInput.value;
+  const title = collectionTitleInput.value;
+  const publishYear = collectionYearInput.value;
+  const numberOfPages = collectionPagesInput.value;
+  newBook = new Book(title, author, publishYear, numberOfPages, statusCheck());
+  return newBook;
+}
 
-displayContact();
+function addCollectionBook() {
+  let collection = [];
+  collection = JSON.parse(localStorage.getItem('collection') || '[]');
+  collection.push(newBook);
+  collection.forEach((book, i) => {
+    book.index = i + 1;
+  });
+  localStorage.setItem('collection', JSON.stringify(collection));
+}
 
-setInterval(displayTime, 1000);
+function hideCollectionForm() {
+  collectionForm.classList.remove('active');
+}
+
+function hideWhislistForm() {
+  whislistForm.classList.remove('active');
+}
+
+addToCollection.addEventListener('click', () => {
+  displayCollectionForm();
+});
+
+addToWhislist.addEventListener('click', () => {
+  displayWhislistForm();
+})
+
+
+submitToCollection.addEventListener('click', (e) => {
+  e.preventDefault();
+  // not applying a form validation to prevent empty input value submits
+  // because input fields will be converted to 'required'
+  createBook();
+  addCollectionBook();
+  displayCollection();
+  setTimeout(hideCollectionForm, 150);
+});
+
+displayCollection();
+
+submitToWhislist.addEventListener('click', (e) => {
+  e.preventDefault();
+  setTimeout(hideWhislistForm, 150);
+})
+
+bookList.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains('remove')) {
+    let collection = JSON.parse(localStorage.getItem('collection') || '[]');
+    collection = collection.filter((book) => book.index !== Number(e.target.id));
+    collection.forEach((book, i) => {
+      book.index = i + 1;
+    });
+    localStorage.setItem('collection', JSON.stringify(collection));
+    displayCollection();
+  }
+  if (e.target.classList.contains('slider')) {
+    const collection = JSON.parse(localStorage.getItem('collection') || '[]');
+    const objIndex = Number(e.target.id);
+    if (collection[objIndex - 1].status === 'Read') {
+      collection[objIndex - 1].status = 'Not read';
+    } else {
+      collection[objIndex - 1].status = 'Read';
+    }
+    localStorage.setItem('collection', JSON.stringify(collection));
+    displayCollection();
+  }
+});
--- modules/displayTime.js
+++
@@ -1,7 +0,0 @@
-import { DateTime } from './luxon.js';
-
-export default function displayTime() {
-  const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-  const timeSlot = document.querySelector('#time');
-  timeSlot.textContent = now.toLocaleString();
-}
